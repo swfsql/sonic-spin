a8e9d67c75c246af943ed797957aa4f1393794bd
Thiago <swfsql@gmail.com>
2019-05-12 23:53:48 -0300

    added reference tests

---
+++ tests/reference.rs
@@ -0,0 +1,26 @@
+#![feature(proc_macro_hygiene)]
+#![allow(unused_parens)]
+
+mod common;
+
+use sonic_spin::sonic_spin;
+
+#[test]
+fn reference_normal() { sonic_spin! {
+    let alt = &4;
+
+    let res = 4::(&);
+
+    assert_eq!(res, &4);
+    assert_eq!(alt, res);
+}}
+
+#[test]
+fn reference_multiple() { sonic_spin! {
+    let alt = &&&4;
+
+    let res = 4::(&)::(&)::(&);
+
+    assert_eq!(res, &&&4);
+    assert_eq!(alt, res);
+}}
