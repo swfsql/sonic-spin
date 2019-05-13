126058765a2fbe994ba6fd18dc2e3cc68c3e4570
Thiago <swfsql@gmail.com>
2019-05-12 23:50:17 -0300

    added box tests

---
+++ tests/box.rs
@@ -0,0 +1,28 @@
+#![feature(proc_macro_hygiene)]
+#![allow(unused_parens)]
+
+#![feature(box_syntax)]
+
+mod common;
+
+use sonic_spin::sonic_spin;
+
+#[test]
+fn box_normal() { sonic_spin! {
+    let alt = box 2;
+
+    let res = 2::(box);
+
+    assert_eq!(res, box 2);
+    assert_eq!(alt, res);
+}}
+
+#[test]
+fn box_let() { sonic_spin! {
+    let alt = box 2;
+
+    2::(box)::(let res =);
+
+    assert_eq!(res, box 2);
+    assert_eq!(alt, res);
+}}
